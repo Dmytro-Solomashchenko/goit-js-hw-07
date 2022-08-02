4033e09829d9839e38fbd795c952a2f7eaccfc95
Dmytro_Solomashchenko <solomashchenko@me.com>
2022-08-02 16:38:49 +0300

    task 2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const imagePaletteContainer = document.querySelector('.gallery');
+const imagesMarkup = createImagesMarkup(galleryItems);
 
-console.log(galleryItems);
+imagePaletteContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+imagePaletteContainer.addEventListener('click', onImagePaletteContainerClick);
+
+function createImagesMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+
+function onImagePaletteContainerClick(evt) {
+  evt.preventDefault();
+  const gallerySwatch = evt.target.classList.contains('gallery__image');
+  if (!gallerySwatch) {
+    return;
+  }
+
+  document.addEventListener('keydown', logKey);
+
+  function logKey(e) {
+    if (e.code === 'Escape') {
+      instance.close();
+    }
+  }
+}
+
+// console.log(galleryItems);
+var lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionDelay: 250,
+});
